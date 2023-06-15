e232ae73392b9175ba11d7da35b0380d92b0c6d0
sabir-shah123 <sabirshahbzu@gmail.com>
2023-06-15 22:39:44 +0500

    shorthen the code

--- src/ChatGPT.php
+++ src/ChatGPT.php
@@ -6,18 +6,12 @@ use GuzzleHttp\Client;
 
 class ChatGPT
 {
-    protected $apiKey;
-    protected $baseUrl;
     protected $client;
-    protected $model;
-
 
     public function __construct()
     {
-
-        $this->baseUrl = 'https://api.openai.com/v1/';
         $this->client = new Client([
-            'base_uri' => $this->baseUrl,
+            'base_uri' => 'https://api.openai.com/v1/',
             'headers' => [
                 'Authorization' => 'Bearer ' . config('chatgpt.api_key'),
                 'Content-Type' => 'application/json',
@@ -25,82 +19,46 @@ class ChatGPT
         ]);
     }
 
-    public function get($endpoint)
+    public function request($method, $endpoint, $data = [])
     {
-        $response = $this->client->get($endpoint);
-        return json_decode($response->getBody()->getContents());
-    }
-
-    public function post($endpoint, $data)
-    {
-        $response = $this->client->post($endpoint, [
+        $response = $this->client->request($method, $endpoint, [
             'json' => $data,
         ]);
+
         return json_decode($response->getBody()->getContents());
     }
 
-    public function Chat($message, $type = 'davinci', $options = [])
+    public function chat($message, $type = 'davinci', $options = [])
     {
         $model = config('chatgpt.model');
+        $url = ($type === 'chat') ? 'chat/completions' : 'completions';
 
-        switch ($type) {
-            case 'chat':
-                $url = 'chat/completions';
-                $data = [
-                    'model' => $model,
-                    'prompt' => $message,
-                    'temperature' => $options['temperature'] ?? 0.5,
-                    'max_tokens' => $options['max_tokens'] ?? 60,
-                    'top_p' => $options['top_p'] ?? 1,
-                    'frequency_penalty' => $options['frequency_penalty'] ?? 0,
-                    'presence_penalty' => $options['presence_penalty'] ?? 0,
-                    'stop' => $options['stop'] ?? null,
-                    'n' => $options['n'] ?? 1,
-                    'stream' => $options['stream'] ?? false,
-                    'logprobs' => $options['logprobs'] ?? null,
-                    'echo' => $options['echo'] ?? false,
-                    'messages' => [
-                        [
-                            'type' => 'text',
-                            'text' => $message,
-                        ]
-                    ],
-                ];
-                break;
+        $data = [
+            'model' => $model,
+            'prompt' => $message,
+            'temperature' => $options['temperature'] ?? 0.5,
+            'max_tokens' => $options['max_tokens'] ?? 60,
+            'top_p' => $options['top_p'] ?? 1,
+            'frequency_penalty' => $options['frequency_penalty'] ?? 0,
+            'presence_penalty' => $options['presence_penalty'] ?? 0,
+            'stop' => $options['stop'] ?? null,
+            'n' => $options['n'] ?? 1,
+            'stream' => $options['stream'] ?? false,
+            'logprobs' => $options['logprobs'] ?? null,
+            'echo' => $options['echo'] ?? false,
+        ];
 
-            case 'davinci':
-            case 'curie':
-            case 'babbage':
-            case 'ada':
-                $url = 'completions';
-                $data = [
-                    'model' => $model, // This should be changed to the appropriate model ID for the chosen API type
-                    'prompt' => $message,
-                    'temperature' => $options['temperature'] ?? 0.5,
-                    'max_tokens' => $options['max_tokens'] ?? 60,
-                    'top_p' => $options['top_p'] ?? 1,
-                    'frequency_penalty' => $options['frequency_penalty'] ?? 0,
-                    'presence_penalty' => $options['presence_penalty'] ?? 0,
-                    'stop' => $options['stop'] ?? null,
-                    'n' => $options['n'] ?? 1,
-                    'stream' => $options['stream'] ?? false,
-                    'logprobs' => $options['logprobs'] ?? null,
-                    'echo' => $options['echo'] ?? false,
-                ];
-                break;
-
-                // Other cases...
-
-            default:
-                throw new Exception("Unsupported API type: $type");
+        if ($type === 'chat') {
+            $data['messages'] = [
+                [
+                    'type' => 'text',
+                    'text' => $message,
+                ],
+            ];
         }
 
-        $response = $this->post($url, $data);
-        return $response;
+        return $this->request('POST', $url, $data);
     }
-
-
-
 
     // Add additional methods as needed
 }
